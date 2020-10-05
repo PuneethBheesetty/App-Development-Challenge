24458ba18af65a14356daa8241e185074da0add9
jack345466 <72225376+jack345466@users.noreply.github.com>
2020-10-05 15:24:47 -0400

    Update readexcel.py

--- readexcel.py
+++ readexcel.py
@@ -1,7 +1,7 @@
 #https://docs.python.org/3/library/csv.html
 import csv
-with open ("fy20_adc_data_file_88_degrees.csv", newline="") as csvfile:
-	for row in csv.reader(csvfile, delimiter=","):
+with open ("fy20_adc_data_file_88_degrees.csv", newline="") as lunar_data:
+	for row in csv.reader(lunar_data):
 		latitude = row[0];
 		longitude = row[1];
 		height = row[2];
