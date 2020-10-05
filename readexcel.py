5fced9fdb468fc118546ff3fe8f17e877354b53b
jack345466 <72225376+jack345466@users.noreply.github.com>
2020-10-05 14:31:02 -0400

    Update readexcel.py

--- readexcel.py
+++ readexcel.py
@@ -1,5 +1,8 @@
 #https://docs.python.org/3/library/csv.html
 import csv
 with open ("fy20_adc_data_file_88_degrees.csv", newline="") as csvfile:
-	for row in cvs.reader(csvfile, delimiter=","):
-		print row;
+	for row in csv.reader(csvfile, delimiter=","):
+		latitude = row[0];
+		longitude = row[1];
+		height = row[2];
+		slope = row[3];
