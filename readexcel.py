244408601e288f4648ff1112ccfc43770ac39a7c
jack345466 <72225376+jack345466@users.noreply.github.com>
2020-10-05 10:12:25 -0400

    Update readexcel.py

--- readexcel.py
+++ readexcel.py
@@ -1,2 +1,10 @@
-#https://openpyxl.readthedocs.io/en/stable/
+#https://openpyxl.readthedocs.io/en/stable/tutorial.html
 from openpyxl import Workbook
+wb = Workbook;
+ws = wb.active;					#get the active worksheet
+for row in ws.rows:			#select one row at a time
+	latitude = row[0];
+	longitude = row[1];
+	height = row[2];
+	slope = row[3];
+	
